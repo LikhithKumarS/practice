a11b7742013eaed6d8a4f439783a6baa81d0d76a
LK <likhithjune@gmail.com>
2025-09-19 23:34:21 +0530

    operators

---
+++ exercise6.py
@@ -0,0 +1,42 @@
+x = 9
+y = 4
+print(x + y)  # addition
+print(x - y)  # sub
+print(x * y)  # multiplication
+print(x / y)  # division
+print(x // y)  # floor
+print(x % y)  # modulo #reminder
+print(x**y)  # power x to y
+
+""" 
+  python exercise6.py
+13
+5
+36
+2.25
+2
+1
+6561
+      """
+
+
+a = -5
+b = 2
+print(a // b)
+
+
+c = 5.0
+d = 2
+print(c // d)
+
+e = 2
+f = -2
+print(e**f)
+
+
+a = 10
+b = 20
+c = 30
+print(a < b and b < c)
+print(a < b or b > c)
+print(not a > b)
