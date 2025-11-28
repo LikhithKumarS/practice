32f64e95ccb322e39256d42e3fc6a2b82f19d04d
LK <likhithjune@gmail.com>
2025-11-28 20:41:37 +0530

    star1

--- Python3/basics_1/exercise12.py
+++ Python3/basics_1/exercise12.py
@@ -5,9 +5,13 @@ else:
 
     print("opp")"""
 
-a = int(input("enter First number"))
-b = int(input("enter Second number"))
-c = int(input("enter Third number"))
+import sys
+
+'''
+a = int(input("enter First number \n"))
+b = int(input("enter Second number \n"))
+c = int(input("enter Third number \t"))
+'''
 ''' 
 if a >= b and a >= c:
     print("a is largest")
@@ -25,4 +29,62 @@ if c is greater than or equal to a AND c is greater than or equal to b
 
 
 '''
+'''
+if a>=b:   #check if a or b if b go to else b>=c
+    if a>=c: #check if a or c print c as iterated with a, b and found c is larger
+        print(a) 
+    else:
+        print(c)
+else:
+    if b>=c:
+        print(b)
+    else:
+        print(c)
+        
+        
+        
+print(max(a,b,c))
+
+'''
+
+
+'''  
+y= int(input("y"))
+if(y%4==0) and (y%100!=0):
+    print("yes")
+elif (y%4==0):
+    print("yes")
+else: 
+    print("nah")
+    
+    
+    '''
+    
+    
+    #calcy
+print(""" 
+      Please select operation:
+      1:ADD
+      2:SUBtract
+      3:MULTIPLY
+      
+      """)
+
+choice=int(input("Select 1,2 OR 3 \n"))
+
+if choice not in (1,2,3):
+    print("Dude cant ya type number")
+    sys.exit()
+
+
+a = int(input("enter First number \n"))
+b = int(input("enter Second number \n"))
+
+if choice ==1:
+    res =a+b
+elif choice ==2:
+    res=a-b
+else:
+    res =a*b
+print("Result is",res)
 
