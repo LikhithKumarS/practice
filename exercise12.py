5b8174f6318ff390e948890eb16c2758b084fa02
LK <likhithjune@gmail.com>
2025-11-28 13:57:38 +0530

    test

--- exercise12.py
+++ exercise12.py
@@ -1,6 +1,28 @@
-n = int(input("enter a number"))
+"""n = int(input("enter a number"))
 if n % 2 == 0:
     print("you")
 else:
-    
-    print("opp")
+
+    print("opp")"""
+
+a = int(input("enter First number"))
+b = int(input("enter Second number"))
+c = int(input("enter Third number"))
+''' 
+if a >= b and a >= c:
+    print("a is largest")
+if b >= a and b>=c:
+    print("b is largest")
+else:
+    print("c is largest")
+
+'''
+'''
+if a is greater than or equal to b AND a is greater than or equal to c
+if b is greater than or equal to a AND b is greater than or equal to c
+if c is greater than or equal to a AND c is greater than or equal to b
+
+
+
+'''
+
