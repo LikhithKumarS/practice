14d57902c7db8fc53be00076e1b70492bfe77ad7
Likhith-QA <likhith@easyship.com>
2025-12-08 12:26:26 +0530

    test

--- Python3/basics_1/exercise14.py
+++ Python3/basics_1/exercise14.py
@@ -11,3 +11,23 @@
 
 
 
+#count number of digits in 9253
+# x = float(input("enter n \n"))
+# res=0
+# while x>0:
+#     x=x//10
+#     print(x,res)
+#     res +=1
+
+# print("the digits",res)
+
+
+
+
+n = int(input("enter n \n"))
+res=1
+print(res,end=" ")
+for i in range(2,n+1):
+    res *= i
+    print(i,res,end=" ")
+print(" factorial is ",res)
